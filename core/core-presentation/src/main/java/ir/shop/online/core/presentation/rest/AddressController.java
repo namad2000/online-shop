baead43f2c061f2c394651f89342eb22f524f5b1
d_akbari <daak1365@gmail.com>
2025-12-08 20:32:59 +0330

    validation ...

--- core/core-presentation/src/main/java/ir/shop/online/core/presentation/rest/AddressController.java
+++ core/core-presentation/src/main/java/ir/shop/online/core/presentation/rest/AddressController.java
@@ -2,9 +2,8 @@ package ir.shop.online.core.presentation.rest;
 
 import ir.shop.online.Infrastructure.mapper.AddressMapper;
 import ir.shop.online.application.dto.req.CreateAddressRequest;
-import ir.shop.online.application.dto.req.CreateUserRequest;
-import ir.shop.online.application.service.AddressService;
-import ir.shop.online.application.service.UserService;
+import ir.shop.online.core.domain.usecase.AddressUseCase;
+import ir.shop.online.core.presentation.rest.dto.AddressDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,13 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AddressController {
 
-    private final AddressService addressService;
+    private final AddressUseCase addressService;
     private final AddressMapper addressMapper;
 
     @PostMapping("/{userId}/add")
-    public ResponseEntity<?> addAddress(@PathVariable("userId") Long userId,
-                                        @RequestBody CreateAddressRequest request) {
-        return ResponseEntity.ok(addressMapper.toDTO(addressService.add(userId, request)));
+    public AddressDTO addAddress(@PathVariable("userId") Long userId,
+                                 @RequestBody CreateAddressRequest request) {
+        return addressMapper.toDTO(addressService.add(userId, request));
     }
 
     @GetMapping("/{userId}/list")
