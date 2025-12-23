a388a8ef4f6cb807d2dd0e89ae5d7df6b7938ba2
m_zahedi <m_zahedi@tamin.org>
2025-12-23 17:57:10 +0330

    fix bugs to build project ...

--- core/core-infrastructure/src/main/java/ir/shop/online/core/infrastructure/config/RedisConfig.java
+++ core/core-infrastructure/src/main/java/ir/shop/online/core/infrastructure/config/RedisConfig.java
@@ -5,26 +5,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, ir.shop.online.core.domain.model.otp.OTP> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, OTP> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, OTP> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
         // Serializer برای کلیدها
         template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
 
-        // Serializer برای مقادیر (JSON)
-        RedisSerializer<OTP> serializer =
-                new JacksonJsonRedisSerializer<>(OTP.class);
-
-        template.setValueSerializer(serializer);
+        // Serializer برای مقادیر (JDK Serialization)
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        template.setHashValueSerializer(new JdkSerializationRedisSerializer());
 
         template.afterPropertiesSet();
         return template;
