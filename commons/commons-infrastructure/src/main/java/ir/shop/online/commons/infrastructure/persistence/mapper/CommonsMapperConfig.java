026efd31a40ad221a2469df726e662451096d634
d_akbari <daak1365@gmail.com>
2025-12-24 16:17:12 +0330

    commons-container ...

--- commons/commons-infrastructure/src/main/java/ir/shop/online/commons/infrastructure/persistence/mapper/CommonsMapperConfig.java
+++ commons/commons-infrastructure/src/main/java/ir/shop/online/commons/infrastructure/persistence/mapper/CommonsMapperConfig.java
@@ -4,8 +4,8 @@ import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
 
 @MapperConfig(
-    componentModel = "spring",
-    unmappedTargetPolicy = ReportingPolicy.IGNORE
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public interface CommonsMapperConfig {
 }
