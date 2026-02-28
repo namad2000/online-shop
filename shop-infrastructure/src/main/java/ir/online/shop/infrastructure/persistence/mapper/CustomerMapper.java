8e4ddbbcef03b8d3e6c878136dafbb887ddb3672
dakbari <daak1365@gmail.com>
2026-02-28 09:27:59 +0330

    CustomerMapper

--- shop-infrastructure/src/main/java/ir/online/shop/infrastructure/persistence/mapper/CustomerMapper.java
+++ shop-infrastructure/src/main/java/ir/online/shop/infrastructure/persistence/mapper/CustomerMapper.java
@@ -1,13 +1,13 @@
 package ir.online.shop.infrastructure.persistence.mapper;
 
-import io.qoop.jpa.persistence.mapper.CommonsInfrastructureMapper;
 import io.qoop.mapper.core.CommonsMapperConfig;
+import io.qoop.mapper.core.mapstruct.BasicMapper;
 import ir.online.shop.domain.model.Customer;
 import ir.online.shop.infrastructure.persistence.entity.CustomerEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(config = CommonsMapperConfig.class)
-public interface CustomerMapper extends CommonsInfrastructureMapper<Customer, CustomerEntity> {
+public interface CustomerMapper extends BasicMapper<Customer, CustomerEntity> {
     Customer toDomain(CustomerEntity entity);
 
     CustomerEntity toEntity(Customer domain);
